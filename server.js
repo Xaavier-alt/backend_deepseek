c337b176437e95f2171bc90ceee6ad719dd09fa6
Xavier <fortuneugwu2@gmail.com>
2025-09-04 23:36:23 +0100

    New Updates

--- server.js
+++ server.js
@@ -70,18 +70,20 @@ passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
 
 // Google Strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      return done(null, profile);
-    }
-  )
-);
+const googleCallbackURL = process.env.GOOGLE_CALLBACK_URL 
+  ? process.env.GOOGLE_CALLBACK_URL.split(',')[0].trim()
+  : '/auth/google/callback';
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: googleCallbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
 
 // Discord Strategy
 passport.use(
